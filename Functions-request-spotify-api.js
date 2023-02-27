d58ebfb63c6eb19b00104ce8e2b1f27949b56447
zeeshanhanif <zeeshanhanif@gmail.com>
2023-02-27 20:23:18 +0500

    Updated api script to include chartmetric api

--- Functions-request-spotify-api.js
+++ Functions-request-spotify-api.js
@@ -1,6 +1,7 @@
 console.log("Spotify API")
 const soundchartsSongId = args[0]
 const songstatsSongId = args[1]
+const chartmetricSongId = args[2]
 
 
 // To make an HTTP request, use the Functions.makeHttpRequest function
@@ -12,6 +13,27 @@ const songstatsSongId = args[1]
 // - data: request body supplied as an object (optional)
 // - timeout: maximum request duration in ms (optional, defaults to 10000ms)
 // - responseType: expected response type (optional, defaults to 'json')+
+
+let chartmetricAuthToken = ""
+const chartmetricAPITokenRequest = Functions.makeHttpRequest({
+  url: `https://api.chartmetric.com/api/token`,
+  method: "POST",
+  data: {
+    "refreshtoken": secrets.chartmetricRefreshToken
+  }
+})
+const [chartmetricAPITokenResponse] = await Promise.all([
+  chartmetricAPITokenRequest
+])
+
+if (!chartmetricAPITokenResponse.error) {
+  chartmetricAuthToken = chartmetricAPITokenResponse.data.token;
+} else {
+  console.log("Chartmetric Auth Token Error ",chartmetricAPITokenResponse);
+  return Functions.encodeUint256(0);
+}
+
+
 
 const spotifyStreamCountRequest = Functions.makeHttpRequest({
   url: `https://customer.api.soundcharts.com/api/v2.24/song/${soundchartsSongId}/spotify/stream`,
@@ -27,10 +49,18 @@ const spotifyStreamCountSongStatsRequest = Functions.makeHttpRequest({
   headers: { "apikey": secrets.songStatsAPIKey  },
 })
 
+const spotifyStreamCountChartmetricRequest = Functions.makeHttpRequest({
+  url: `https://api.chartmetric.com/api/track/${chartmetricSongId}/spotify/stats?type=streams&latest=true`,
+  headers: { "Authorization": `Bearer ${chartmetricAuthToken}`  },
+})
+
+
+
 // First, execute all the API requests are executed concurrently, then wait for the responses
-const [spotifyStreamCountResponse, spotifyStreamCountSongStatsResponse] = await Promise.all([
+const [spotifyStreamCountResponse, spotifyStreamCountSongStatsResponse, spotifyStreamCountChartmetricResponse] = await Promise.all([
   spotifyStreamCountRequest,
-  spotifyStreamCountSongStatsRequest
+  spotifyStreamCountSongStatsRequest,
+  spotifyStreamCountChartmetricRequest
 ])
 
 //console.log("spotifyStreamCountResponse = ",spotifyStreamCountResponse);
@@ -42,18 +72,28 @@ console.log("spotifyStreamCountResponse value = ",spotifyStreamCountResponse.dat
 //console.log("spotifyStreamCountSongStatsResponse.data = ",spotifyStreamCountSongStatsResponse.data.stats[0]);
 console.log("spotifyStreamCountSongStatsResponse streams_total = ",spotifyStreamCountSongStatsResponse.data.stats[0].data.streams_total);
 
+//console.log("spotifyStreamCountChartmetricResponse = ",spotifyStreamCountChartmetricResponse);
+//console.log("spotifyStreamCountChartmetricResponse.data = ",spotifyStreamCountChartmetricResponse.data.obj[0]);
+console.log("spotifyStreamCountChartmetricResponse streams count = ",spotifyStreamCountChartmetricResponse.data.obj[0].value);
+
 const stats = [];
 
 if (!spotifyStreamCountResponse.error) {
   stats.push(spotifyStreamCountResponse.data.items[0].plots[0].value)
 } else {
-  console.log("SoundCharts Spotify Listeners Count Error")
+  console.log("SoundCharts Spotify Stream Count Error")
 }
 
 if (!spotifyStreamCountSongStatsResponse.error) {
   stats.push(spotifyStreamCountSongStatsResponse.data.stats[0].data.streams_total)
 } else {
-  console.log("Song Stats Spotify Listeners Count Error")
+  console.log("Song Stats Spotify Stream Count Error")
+}
+
+if (!spotifyStreamCountChartmetricResponse.error) {
+  stats.push(spotifyStreamCountChartmetricResponse.data.obj[0].value)
+} else {
+  console.log("Chartmetric Spotify Stream Count Error")
 }
 
 /*
@@ -63,8 +103,9 @@ if (prices.length < 3) {
   throw Error("More than 1 API failed")
 }
 */
-
-const medianStats = stats.sort((a, b) => a - b)[Math.round(stats.length / 2)]
+console.log(`sorted: ${stats.sort((a, b) => a - b)}`);
+console.log(`middle number: ${Math.floor(stats.length / 2)}`);
+const medianStats = stats.sort((a, b) => a - b)[Math.floor(stats.length / 2)]
 console.log(`Median Spotify Listeners: ${medianStats}`)
 
 // The source code MUST return a Buffer or the request will return an error message
