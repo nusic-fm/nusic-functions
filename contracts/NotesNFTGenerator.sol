db116287f28737aa021d6deaffbd1e7b2fe19dde
zeeshanhanif <zeeshanhanif@gmail.com>
2023-03-01 00:05:00 +0500

    Updated NotesNFTGenerator to include MockUSDC and manager address

--- contracts/NotesNFTGenerator.sol
+++ contracts/NotesNFTGenerator.sol
@@ -7,8 +7,8 @@ import "./NotesNFT.sol";
 
 contract NotesNFTGenerator is Ownable {
     
-    function generateNFT(string memory _notesName, string memory _notesSymbol) public returns(address) {
-        NotesNFT nft = new NotesNFT(_notesName, _notesSymbol);
+    function generateNFT(string memory _notesName, string memory _notesSymbol, address _usdcAddress, address _manager) public returns(address) {
+        NotesNFT nft = new NotesNFT(_notesName, _notesSymbol, _usdcAddress, _manager);
         return address(nft);
     }
 }
